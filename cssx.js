2ed2bdc5dc165debf4548debcba3a5ad657651bd
galulex <galulex@gmail.com>
2024-07-23 15:22:39 +0300

    Adds code highllights

--- cssx.js
+++ cssx.js
@@ -23,3 +23,33 @@ document.addEventListener('input', cssx)
 document.addEventListener('mousemove', cssx)
 document.addEventListener('click', cssx)
 document.addEventListener('mouseenter', cssx)
+
+const events = [
+    "DOMContentLoaded", // Fired when the initial HTML document has been completely loaded and parsed
+    "readystatechange", // Fired when the ready state of the document changes
+    "load", // Fired when the whole page has loaded, including all dependent resources
+    "beforeunload", // Fired when the window, the document and its resources are about to be unloaded
+    "unload", // Fired when the document or a resource is being unloaded
+    "abort", // Fired when the loading of a resource has been aborted
+    "error", // Fired when an error occurs while loading an external file
+    "resize", // Fired when the document view (window) has been resized
+    "scroll", // Fired when the document view or an element has been scrolled
+    "focus", // Fired when the document or an element gains focus
+    "blur", // Fired when the document or an element loses focus
+    "click", // Fired when a mouse click occurs on the document or an element
+    "dblclick", // Fired when a mouse double-click occurs on the document or an element
+    "mousedown", // Fired when a mouse button is pressed on the document or an element
+    "mouseup", // Fired when a mouse button is released on the document or an element
+    "mousemove", // Fired when the mouse pointer is moved over the document or an element
+    "mouseover", // Fired when the mouse pointer is moved onto an element
+    "mouseout", // Fired when the mouse pointer is moved out of an element
+    "keypress", // Fired when a key is pressed down and released
+    "keydown", // Fired when a key is pressed down
+    "keyup", // Fired when a key is released
+    "touchstart", // Fired when a touch point is placed on the touch surface
+    "touchmove", // Fired when a touch point is moved along the touch surface
+    "touchend", // Fired when a touch point is removed from the touch surface
+    "touchcancel" // Fired when a touch point has been disrupted in some way
+];
+
+events.forEach(e => document.addEventListener(e, cssx))
